20cb8fa2f81e8ee252d3ff6c73d40ce8b043260a
Onke Tshaka <21819755@dut4life.ac.za>
2021-02-08 15:59:57 +0200

    Add project files.

---
+++ guessingGame/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace guessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int secret = 1;
+            int guess = 0;
+            int guestCount = 0;
+            int guestLimit = 3;
+            bool outOfGuesses = false;
+            while(secret != guess && !outOfGuesses)
+            {
+                if(guestCount < guestLimit)
+                {
+                    Console.Write("Enter a number between 1-10: ");
+                    guess = Convert.ToInt32(Console.ReadLine());
+                    guestCount++;
+                }
+                else
+                {
+                    outOfGuesses = true;
+                }
+              
+            }
+            if (outOfGuesses)
+            {
+                Console.Write("You Lose!");
+                Console.ReadLine();
+            }
+            else
+            {
+                Console.Write("You Win!");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
